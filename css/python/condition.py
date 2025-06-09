bc49d36131bb6cede4bd869d02fdae809e9556e0
harini <hariniviswanathan91@gmail.com>
2025-06-09 16:29:47 +0530

    made changes

---
+++ css/python/condition.py
@@ -0,0 +1,28 @@
+a=10
+b=20
+c=60
+if a>b or a<b:
+    print("a is greater")
+elif b>a or b>c:
+    print("b is greater")
+else:
+    print("b is greater")
+for i in range (2,10,2):
+        print(i)
+def add ():
+    print(10+2)
+add()
+list = [1,2,3,4,5];
+for i in list:
+    print(i)
+    tuple=(1,2,3,4,5,6);
+    for i in tuple:
+        print(i)
+print(type(list))
+
+dictionary={"name":"haryyy","password":"20062007"}
+print(dictionary["name"])
+print(type(dictionary))
+for key in dictionary:
+    print(dictionary[key])
+    
---
+++ css/python/demo.py
@@ -0,0 +1,11 @@
+a=20
+b=10
+print(a+b)
+print(type(a))
+c="string"
+d=True
+pi=3.14
+print(type(c))
+print(type(d))
+print(type(pi))
+
